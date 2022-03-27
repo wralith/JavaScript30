6660fbfe99cbe46e1e84d02c698247b01a5507d4
Wralith <wralithdev@gmail.com>
2022-03-27 04:53:07 +0300

    Add Day6

---
+++ 06 - Type Ahead/README.md
@@ -0,0 +1,6 @@
+## Javascript 30 - Day 6
+Ajax Type Ahead
+
+Additionally i added color-switch.
+
+6th day of 30 Day Vanilla JS Coding Challenge in https://javascript30.com/.
---
+++ 06 - Type Ahead/app.js
@@ -0,0 +1,65 @@
+const endpoint =
+  "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+
+const cities = [];
+fetch(endpoint).then((blob) =>
+  blob.json().then((data) => cities.push(...data))
+);
+
+function findMatches(wordToMatch, cities) {
+  return cities.filter((place) => {
+    const regex = new RegExp(wordToMatch, "gi");
+    return place.city.match(regex) || place.state.match(regex);
+  });
+}
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function displayMatches() {
+  const matchArray = findMatches(this.value, cities);
+  const html = matchArray
+    .map((place) => {
+      const regex = new RegExp(this.value, "gi");
+      const cityName = place.city.replace(
+        regex,
+        `<span class='hl'>${this.value}</span>`
+      );
+      const stateName = place.state.replace(
+        regex,
+        `<span class='hl'>${this.value}</span>`
+      );
+
+      return `
+    <li>
+    <span class='name'>${cityName}, ${stateName}</span>
+    <span class='population'>${numberWithCommas(place.population)}</span>
+    </li>
+    `;
+    })
+    .join("");
+
+  suggestions.innerHTML = html;
+}
+
+const inputArea = document.querySelector(".search");
+const suggestions = document.querySelector(".suggestions");
+const input = inputArea.value;
+inputArea.addEventListener("change", displayMatches);
+inputArea.addEventListener("keyup", displayMatches);
+
+const colorModeBtn = document.querySelector(".btn-switch");
+
+let orange = true;
+function setBackgroundColor() {
+  if (orange) {
+    orange = false;
+    document.documentElement.style.setProperty("--color", "#372b57");
+  } else {
+    orange = true;
+    document.documentElement.style.setProperty("--color", "#ffc600");
+  }
+}
+
+colorModeBtn.addEventListener("click", setBackgroundColor);
