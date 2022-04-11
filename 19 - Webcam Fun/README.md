31c6d222783204b106e807fb78f8eacf27785367
Wralith <wralithdev@gmail.com>
2022-04-11 23:43:26 +0300

    Add Day 19

---
+++ 19 - Webcam Fun/README.md
@@ -0,0 +1,10 @@
+## Javascript 30 - Day 19
+Webcam
+Lots of Fun.
+
+Need to <br>
+`npm install` <br>
+`npm run` <br>
+to get it work.
+
+19th day of 30 Day Vanilla JS Coding Challenge in https://javascript30.com/.
---
+++ 19 - Webcam Fun/scripts.js
@@ -0,0 +1,50 @@
+const video = document.querySelector(".player");
+const canvas = document.querySelector(".photo");
+const ctx = canvas.getContext("2d");
+const strip = document.querySelector(".strip");
+const snap = document.querySelector(".snap");
+
+function getVideo() {
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    .then((localMediaStream) => {
+      console.log(localMediaStream);
+
+      //Did not worked. Deprecated
+      // video.src = window.URL.createObjectURL(localMediaStream);
+
+      video.srcObject = localMediaStream;
+      video.play();
+    })
+    .catch((err) => {
+      "Webcam Access Error!", err;
+    });
+}
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 20);
+}
+
+function takePhoto() {
+  snap.currentTime = 0;
+  snap.play();
+
+  const photoData = canvas.toDataURL("image/jpeg");
+  const link = document.createElement("a");
+  link.href = photoData;
+  link.setAttribute("download", "pic");
+  link.innerHTML = `<img src="${photoData}" alt="Photo" />`;
+  // link.textContent = "Download Image";
+  strip.insertBefore(link, strip.firstChild);
+}
+
+getVideo();
+
+video.addEventListener("canplay", paintToCanvas);
